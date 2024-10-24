d94723327bfcb8c17dd37cb32d812a01c3f9fb6b
serincubus <serincubus2@gmail.com>
2024-10-24 16:10:21 -0300

    creando el modelo ArticulosInformativos.js

---
+++ src/database/models/articulos_informativos.js
@@ -0,0 +1,45 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    class ArticulosInformativos extends Model {}
+    
+    ArticulosInformativos.init({
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        fecha: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        titulo: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        imagen: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        epigrafe: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        partido: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        texto_informativo: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    }, {
+        sequelize,
+        modelName: 'ArticulosInformativos',
+        tableName: 'articulos_informativos',
+        timestamps: false 
+    });
+
+    return ArticulosInformativos;
+};
